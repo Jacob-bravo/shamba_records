5c7b97069b9e3e0e12ffaf471fd82cf769f6a7dc
jakebravo <jacobmuthondio@gmail.com>
2026-04-20 20:02:10 +0300

    md link

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,11 +3,7 @@ import { ENV } from "./env";
 
 export const connectDB = async (): Promise<void> => {
   try {
-    if (!ENV.MONGO_URI) {
-      console.error("❌ MONGO_URI is not defined in environment variables");
-      process.exit(1);
-    }
-    const connection = await mongoose.connect(ENV.MONGO_URI);
+    const connection = await mongoose.connect('mongodb+srv://jakebravoke_db_user:gLEb5HF5Iq0R1og5@cluster0.ug7vlno.mongodb.net/FieldScale?retryWrites=true&w=majority&appName=Cluster0');
     console.log("MongoDB connected successfully");
     console.log(`Database: ${connection.connection.name}`);
     console.log(`Host: ${connection.connection.host}`);
